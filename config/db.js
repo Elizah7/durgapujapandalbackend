734ac6d3329ff06aec602568b4bda875eeb90423
Uddeshy Manoj Jaiswal <uddeshkrrish@gmail.com>
2023-08-12 17:54:08 +0530

    changedmongourlintoMONGO_URL

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 require("dotenv").config()
 
 
-const connection = mongoose.connect(process.env.mongourl)
+const connection = mongoose.connect(process.env.MONGO_URL)
 
 
 module.exports = {
