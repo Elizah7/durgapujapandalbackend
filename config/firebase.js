59b88d713ca7473abbe4d74e72a318c118641a2d
Uddeshy Manoj Jaiswal <uddeshkrrish@gmail.com>
2023-08-12 18:15:54 +0530

    changedfirebaseconfig

--- config/firebase.js
+++ config/firebase.js
@@ -5,12 +5,12 @@ require("dotenv").config()
 
 module.exports = {
     firebaseConfig: {
-        apiKey: process.env.apiKey,
-        authDomain: process.env.authDomain,
-        projectId: process.env.projectId,
-        storageBucket: process.env.storageBucket,
-        messagingSenderId: process.env.messagingSenderId,
-        appId: process.env.appId
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGINGSENDERID,
+        appId: process.env.APPID
     }
 }
   // Initialize Firebase
